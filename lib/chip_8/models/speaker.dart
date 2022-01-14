1a80a072607887dd6b43705907ca84887db3ed46
Ravi Dev Pandey <pandeyravidev@gmail.com>
2022-01-14 21:00:46 +0530

    some refactoring

--- lib/chip_8/models/speaker.dart
+++ lib/chip_8/models/speaker.dart
@@ -2,16 +2,16 @@ import 'package:just_audio/just_audio.dart';
 
 class Speaker {
   static final _beepPlayer = AudioPlayer();
-  static final _musicPlayer = AudioPlayer();
+  // static final _musicPlayer = AudioPlayer();
 
   static Future<void> load() async {
     await _beepPlayer.setAsset(
       'assets/sound/pickupCoin(8).wav',
     );
 
-    await _musicPlayer.setAsset(
-      'assets/sound/beep.wav',
-    );
+    // await _musicPlayer.setAsset(
+    //   'assets/sound/beep.wav',
+    // );
   }
 
   static Future<void> play() async {
